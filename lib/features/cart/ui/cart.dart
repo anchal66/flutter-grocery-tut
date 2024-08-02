b98bb14df8f0ebad4b877362e9efff4a226e8cd6
Avinash Anchal <anchaavi@amazon.com>
2024-08-02 22:38:21 +0530

    Impl bloc for cart

--- lib/features/cart/ui/cart.dart
+++ lib/features/cart/ui/cart.dart
@@ -1,4 +1,7 @@
+import 'package:bloc_tet/features/cart/bloc/cart_bloc.dart';
+import 'package:bloc_tet/features/cart/ui/cart_tile.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class Cart extends StatefulWidget {
   const Cart({super.key});
@@ -8,11 +11,39 @@ class Cart extends StatefulWidget {
 }
 
 class _CartState extends State<Cart> {
+  final CartBloc cartBloc = CartBloc();
+
+  @override
+  void initState() {
+    cartBloc.add(CartInitialEvent());
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Cart"),
+        title: Text("Cart Items"),
+      ),
+      body: BlocConsumer<CartBloc, CartState>(
+        bloc: cartBloc,
+        listener: (context, state) {},
+        listenWhen: (pre, curr) => curr is CartActionState,
+        buildWhen: (pre, curr) => curr is! CartActionState,
+        builder: (context, state) {
+          switch (state.runtimeType) {
+            case CartSuccessState:
+              final successState = state as CartSuccessState;
+              return ListView.builder(
+                  itemCount: successState.cartItems.length,
+                  itemBuilder: (context, index) {
+                    return CartTileWidget(
+                        cartBloc: cartBloc,
+                        productDataModel: successState.cartItems[index]);
+                  });
+          }
+          return Container();
+        },
       ),
     );
   }
---
+++ lib/features/cart/ui/cart_tile.dart
@@ -0,0 +1,77 @@
+import 'package:bloc_tet/features/cart/bloc/cart_bloc.dart';
+import 'package:bloc_tet/features/home/bloc/home_bloc.dart';
+import 'package:bloc_tet/features/home/models/home_product_data_model.dart';
+import 'package:flutter/material.dart';
+
+class CartTileWidget extends StatelessWidget {
+  final ProductDataModel productDataModel;
+  final CartBloc cartBloc;
+
+  const CartTileWidget(
+      {super.key, required this.productDataModel, required this.cartBloc});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(10),
+      padding: EdgeInsets.all(10),
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          border: Border.all(color: Colors.black)),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            height: 200,
+            width: double.maxFinite,
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                fit: BoxFit.cover,
+                image: NetworkImage(productDataModel.imageUrl),
+              ),
+            ),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          Text(
+            productDataModel.name,
+            style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+          ),
+          Text(productDataModel.description),
+          const SizedBox(
+            height: 20,
+          ),
+          Row(
+            children: [
+              Text(
+                '\$ ${productDataModel.price}',
+                style:
+                const TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  IconButton(
+                    onPressed: () {
+                      // homeBloc.add(HomeProductWishListButtonCLickedEvent(
+                      //     clickedProduct: productDataModel));
+                    },
+                    icon: Icon(Icons.favorite_border),
+                  ),
+                  IconButton(
+                    onPressed: () {
+                      // homeBloc.add(HomeProductCartButtonCLickedEvent(
+                      //     clickedProduct: productDataModel));
+                    },
+                    icon: Icon(Icons.shopping_bag),
+                  ),
+                ],
+              )
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
